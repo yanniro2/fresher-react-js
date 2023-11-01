f68b23a780e06d0e6473d96b07959c4c472a06dc
Niroyan Kugaruban <48611787+yanniro2@users.noreply.github.com>
2023-11-01 09:43:33 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="w-screen h-screen flex items-center justify-center bg-gray-100">{children}</body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,13 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: [
+    'hidden',
+    'block',
+    'brightness-50',
+   'hover:brightness-75',
+    
+  ],
   theme: {
     extend: {
       backgroundImage: {
