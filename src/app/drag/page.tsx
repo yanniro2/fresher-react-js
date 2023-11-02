2c2974a826037ba34cfd45c25df1d6cdd1414cca
Niroyan Kugaruban <48611787+yanniro2@users.noreply.github.com>
2023-11-02 09:18:41 +0530

    drag and drop fix

--- src/app/drag/page.tsx
+++ src/app/drag/page.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
 
-const page = () => {
-  return <div></div>;
+const Page = () => {
+  const [people, setPeople] = useState([
+    { id: 1, name: "John Doe", content: "Lorem 1" },
+    { id: 2, name: "Jane Doe", content: "Lorem 2" },
+    { id: 3, name: "James Doe", content: "Lorem 3" },
+    { id: 4, name: "Jade Doe", content: "Lorem 4" },
+    { id: 5, name: "Jake Doe", content: "Lorem 5" },
+  ]);
+
+  const dragPerson = useRef<number>(0);
+  const dragOverPerson = useRef<number>(0);
+
+  const handleSort = () => {
+    const peopleClone = [...people];
+    const temp = peopleClone[dragPerson.current];
+    peopleClone[dragPerson.current] = peopleClone[dragOverPerson.current];
+    peopleClone[dragOverPerson.current] = temp;
+    setPeople(peopleClone);
+  };
+  return (
+    <div className="flex flex-col gap-[1rem]">
+      {people.map((data, index) => (
+        <div
+          key={index}
+          className=" flex item-center justify-center rounded drop-shadow bg-white p-5 text-center"
+          draggable
+          onDragStart={() => (dragPerson.current = index)}
+          onDragEnter={() => (dragOverPerson.current = index)}
+          onDragEnd={handleSort}
+          onDragOver={(e) => e.preventDefault()}>
+          <p>{data.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default page;
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,200 +1,175 @@
 "use client"
 
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
-
- const imagesData: ImageData[] = [
-   {
-     id: 1,
-     imgUrl: "/images/image-1.webp",
-   },
-   {
-     id: 2,
-     imgUrl: "/images/image-2.webp",
-   },
-   {
-     id: 3,
-     imgUrl: "/images/image-3.webp",
-   },
-   {
-     id: 4,
-     imgUrl: "/images/image-4.webp",
-   },
-   {
-     id: 5,
-     imgUrl: "/images/image-5.webp",
-   },
-   {
-     id: 6,
-     imgUrl: "/images/image-6.webp",
-   },
-   {
-     id: 7,
-     imgUrl: "/images/image-7.webp",
-   },
-   {
-     id: 8,
-     imgUrl: "/images/image-8.webp",
-   },
-   {
-     id: 9,
-     imgUrl: "/images/image-9.webp",
-   },
-   {
-     id: 10,
-     imgUrl: "/images/image-10.jpeg",
-   },
-   {
-     id: 11,
-     imgUrl: "/images/image-11.jpeg",
-   },
- ];
-  
-
-  interface ImageData {
-    id: number;
-    imgUrl: string;
-  }
-  
-  
-
-
+interface ImageData {
+  id: number;
+  imgUrl: string;
+}
 
 export default function Home() {
-  const [active, setActive] = useState(11);
+  const [imagesData, setImagesData] = useState<ImageData[]>([
+    {
+      id: 1,
+      imgUrl: "/images/image-1.webp",
+    },
+    {
+      id: 2,
+      imgUrl: "/images/image-2.webp",
+    },
+    {
+      id: 3,
+      imgUrl: "/images/image-3.webp",
+    },
+    {
+      id: 4,
+      imgUrl: "/images/image-4.webp",
+    },
+    {
+      id: 5,
+      imgUrl: "/images/image-5.webp",
+    },
+    {
+      id: 6,
+      imgUrl: "/images/image-6.webp",
+    },
+    {
+      id: 7,
+      imgUrl: "/images/image-7.webp",
+    },
+    {
+      id: 8,
+      imgUrl: "/images/image-8.webp",
+    },
+    {
+      id: 9,
+      imgUrl: "/images/image-9.webp",
+    },
+    {
+      id: 10,
+      imgUrl: "/images/image-10.jpeg",
+    },
+    {
+      id: 11,
+      imgUrl: "/images/image-11.jpeg",
+    },
+  ]);
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
-   const [selectAll, setSelectAll] = useState(false);
+  const [selectAll, setSelectAll] = useState(false);
 
-   const mainImg = imagesData.filter((item) => item.id === active);
-   const subImg = imagesData.filter((item) => item.id !== active);
+  const dragPerson = useRef<number>(0);
+  const dragOverPerson = useRef<number>(0);
 
-   const handleCheckboxChange = (
-     event: React.ChangeEvent<HTMLInputElement>
-   ) => {
-     const { checked, name } = event.target;
-     if (name === "select") {
-       setSelectAll(checked);
-       if (checked) {
-         const allUrls = imagesData.map((item) => item.imgUrl);
-         setSelectedItems(allUrls);
-       } else {
-         setSelectedItems([]);
-       }
-     } else {
-       let updatedSelectedItems = [];
-       if (checked) {
-         updatedSelectedItems = [...selectedItems, name];
-       } else {
-         updatedSelectedItems = selectedItems.filter((item) => item !== name);
-       }
-       setSelectedItems(updatedSelectedItems);
-       setSelectAll(updatedSelectedItems.length === imagesData.length);
-     }
-   };
+  const handleSort = () => {
+    const imagesDataClone = [...imagesData];
+    const temp = imagesDataClone[dragPerson.current];
+    imagesDataClone[dragPerson.current] =
+      imagesDataClone[dragOverPerson.current];
+    imagesDataClone[dragOverPerson.current] = temp;
+    setImagesData(imagesDataClone);
+  };
 
-   return (
-     <main className="container bg-white rounded-lg drop-shadow">
-       {selectedItems.length > 0 ? (
-         <header className="p-5 flex w-full h-[4rem] border-b justify-between items-center">
-           <div className="flex items-center gap-5">
-             <input
-               type="checkbox"
-               name="select"
-               id="select"
-               checked={selectAll}
-               onChange={handleCheckboxChange}
-             />
-             <label
-               htmlFor="select"
-               className="flex items-center gap-2 font-bold">
-               <span>{selectedItems.length}</span> Files Selected
-             </label>
-           </div>
-           <button className="text-red-600 font-semibold">Delete files</button>
-         </header>
-       ) : (
-         <header className="p-5 w-full h-[4rem] border-b font-bold">
-           Gallery
-         </header>
-       )}
-       <main className="w-full h-[80vh] p-5 grid grid-rows-3 grid-cols-5 gap-4">
-         {mainImg.map((data) => (
-           <div
-             key={data.imgUrl}
-             className={`${
-               selectedItems.includes(data.imgUrl)
-                 ? "brightness-50"
-                 : "hover:brightness-75"
-             } hover:brightness-50 transition group col-span-2 row-span-2 `}>
-             <label htmlFor={data.imgUrl}>
-               <Image
-                 src={data.imgUrl}
-                 alt="img url"
-                 width={400}
-                 height={400}
-                 className="w-full h-full object-cover bg-white text-center rounded-lg drop-shadow"
-                 key={data.imgUrl}
-               />
-             </label>
-             <input
-               type="checkbox"
-               name={data.imgUrl}
-               id={data.imgUrl}
-               className={`${
-                 selectedItems.includes(data.imgUrl)
-                   ? "block"
-                   : "hidden group-hover:block "
-               } absolute z-10 top-5 left-5`}
-               onChange={handleCheckboxChange}
-               checked={selectAll || selectedItems.includes(data.imgUrl)}
-             />
-             {/* {selectedItems.includes(data.imgUrl) && (
-               <p className="text-green-500">Selected</p>
-             )} */}
-           </div>
-         ))}
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked, name } = event.target;
+    if (name === "select") {
+      setSelectAll(checked);
+      if (checked) {
+        const allUrls = imagesData.map((item) => item.imgUrl);
+        setSelectedItems(allUrls);
+      } else {
+        setSelectedItems([]);
+      }
+    } else {
+      let updatedSelectedItems = [];
+      if (checked) {
+        updatedSelectedItems = [...selectedItems, name];
+      } else {
+        updatedSelectedItems = selectedItems.filter((item) => item !== name);
+      }
+      setSelectedItems(updatedSelectedItems);
+      setSelectAll(updatedSelectedItems.length === imagesData.length);
+    }
+  };
 
-         {subImg.map((data) => (
-           <div
-             key={data.imgUrl}
-             className={`${
-               selectedItems.includes(data.imgUrl)
-                 ? "brightness-50"
-                 : "hover:brightness-75"
-             } bg-white p-4 text-center rounded-lg drop-shadow transition group`}>
-             <label htmlFor={data.imgUrl}>
-               <Image
-                 src={data.imgUrl}
-                 alt="img url"
-                 width={100}
-                 height={100}
-                 className="w-full h-full object-cover"
-               />
-             </label>
+  return (
+    <main className="container bg-white rounded-lg drop-shadow">
+      {selectedItems.length > 0 ? (
+        <header className="p-5 flex w-full h-[4rem] border-b justify-between items-center">
+          <div className="flex items-center gap-5">
+            <input
+              type="checkbox"
+              name="select"
+              id="select"
+              checked={selectAll}
+              onChange={handleCheckboxChange}
+            />
+            <label
+              htmlFor="select"
+              className="flex items-center gap-2 font-bold">
+              <span>{selectedItems.length}</span> Files Selected
+            </label>
+          </div>
+          <button className="text-red-600 font-semibold">Delete files</button>
+        </header>
+      ) : (
+        <header className="p-5 w-full h-[4rem] border-b font-bold">
+          Gallery
+        </header>
+      )}
+      <main className="w-full h-[80vh] p-5 grid grid-rows-3 grid-cols-5 gap-4">
+        {imagesData.map((data, index) => {
+          const isFirstIndex = index === 0;
+          const isSelected = selectedItems.includes(data.imgUrl);
+          const brightnessClass = isFirstIndex
+            ? isSelected
+              ? "brightness-50"
+              : "hover:brightness-75 hover:brightness-50 transition group col-span-2 row-span-2"
+            : isSelected
+            ? "brightness-50"
+            : "hover:brightness-75 bg-white p-4 text-center rounded-lg drop-shadow transition group";
 
-             <input
-               type="checkbox"
-               name={data.imgUrl}
-               id={data.imgUrl}
-               className={`${
-                 selectedItems.includes(data.imgUrl)
-                   ? "block"
-                   : "hidden group-hover:block "
-               } absolute z-10 top-5 left-5`}
-               onChange={handleCheckboxChange}
-               checked={selectAll || selectedItems.includes(data.imgUrl)}
-             />
-             {/* {selectedItems.includes(data.imgUrl) && (
-              <p className="text-green-500">Selected</p>
-            )} */}
-           </div>
-         ))}
+          return (
+            <div
+              key={data.imgUrl}
+              className={brightnessClass}
+              draggable
+              onDragStart={() => (dragPerson.current = index)}
+              onDragEnter={() => (dragOverPerson.current = index)}
+              onDragEnd={handleSort}
+              onDragOver={(e) => e.preventDefault()}>
+              <label htmlFor={data.imgUrl}>
+                <Image
+                  src={data.imgUrl}
+                  alt="img url"
+                  width={isFirstIndex ? 400 : 100}
+                  height={isFirstIndex ? 400 : 100}
+                  className={
+                    isFirstIndex
+                      ? "w-full h-full object-cover bg-white text-center rounded-lg drop-shadow"
+                      : "w-full h-full object-cover"
+                  }
+                  key={data.imgUrl}
+                />
+              </label>
+              <input
+                type="checkbox"
+                name={data.imgUrl}
+                id={data.imgUrl}
+                className={`${
+                  isSelected ? "block" : "hidden group-hover:block"
+                } absolute z-10 top-5 left-5`}
+                onChange={handleCheckboxChange}
+                checked={selectAll || isSelected}
+              />
+            </div>
+          );
+        })}
 
-         <div className="capitalize bg-gray-100 rounded-lg flex items-center justify-center border-dashed border cursor-pointer">
-           add images
-         </div>
-       </main>
-     </main>
-   );
+        <div className="capitalize bg-gray-100 rounded-lg flex items-center justify-center border-dashed border cursor-pointer">
+          add images
+        </div>
+      </main>
+    </main>
+  );
 }
